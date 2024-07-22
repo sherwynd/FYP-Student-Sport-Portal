bb4aee565754d5adaeb2ad92e2f717b857a0b245
Sherwynd Liew <sherwynd123@gmail.com>
2024-07-22 14:02:12 +0800

    Header added

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,5 +1,8 @@
 export default function Header() {
   return (
-    <header className="flex justify-center bg-sky-300">Header Page</header>
+    <header className="grid gap-4 bg-sky-300 md:grid-cols-3">
+      <img src="HeaderLogo.png" alt="" width={80} className="m-2" />
+      <h1 className="">Student Sport Portal</h1>
+    </header>
   );
 }
