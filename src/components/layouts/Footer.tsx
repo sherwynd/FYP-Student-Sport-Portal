f94262f6eeaf415983783aa6494592932bf17a71
Sherwynd Liew <sherwynd123@gmail.com>
2024-07-22 14:02:37 +0800

    Footer added

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
     <footer className="flex justify-center bg-stone-900 p-3">
-      <p className="text-neutral-50">Footer Page</p>
+      <ul className="text-neutral-50">
+        <li>Privacy</li>
+        <li>Terms</li>
+      </ul>
+      <p className="text-neutral-50">Footer </p>
     </footer>
   );
 }
