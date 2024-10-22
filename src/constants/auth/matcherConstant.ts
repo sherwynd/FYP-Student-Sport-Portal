dc5b0e28b3f577c1183150464ae366e65a5e9e15
Sherwynd Liew <sherwynd123@gmail.com>
2024-10-23 01:14:03 +0800

    Auth : middleware updated Constants : matcherConstant added Other : loginSchema moved Config : shortcut routes added

---
+++ src/constants/auth/matcherConstant.ts
@@ -0,0 +1,5 @@
+export const MatcherConfig = {
+  staticFiles:
+    "(?!_next[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2|ico|csv|docx?|xlsx?|zip|webmanifest).*)",
+  apiRoutes: "/(api|trpc)(.*)",
+};
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,11 @@
 export { auth as middleware } from "@/auth";
+import { MatcherConfig } from "@constants/auth/matcherConstant";
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    MatcherConfig.staticFiles,
+    // Always run for API routes
+    MatcherConfig.apiRoutes,
+  ],
+};
