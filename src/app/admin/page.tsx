b77f4121875521ae41255bc73133dd78b88d56a2
Sherwynd Liew <sherwynd123@gmail.com>
2024-11-12 01:25:52 +0800

    Data Table added

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,26 @@
 // import { auth } from "@/auth";
 // import { redirect } from "next/navigation";
+import prisma from "@/databases/db";
+// import TestChart from "@/components/chart/TestChart";
+import { DataTable } from "@/components/ui/DataTable";
+import { AdminEventDataColumns } from "@/components/admin/AdminTableColumns";
 
 export default async function Admin() {
+  const eventData = await prisma.event.findMany();
   // const session = await auth();
   // const user = session?.user;
   // if (user?.role === "admin") {
   //   redirect("/");
   // }
-  return <h1>Admin Page</h1>;
+  return (
+    <main className="flex items-center justify-center">
+      {/* Data Table */}
+      <DataTable columns={AdminEventDataColumns} data={eventData} />
+
+      {/* Visualization Board */}
+      {/* <div className="chart-table min-h-min w-full">
+        <TestChart />
+      </div> */}
+    </main>
+  );
 }
---
+++ src/components/admin/AdminTableColumns.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal, ArrowUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DataTableColumnHeader } from "@/components/ui/DataTableColumnHeader ";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type AdminEvent = {
+  id: string;
+  slug: string;
+  title: string;
+  description: string;
+  courseLevel: string;
+  creditHour: number;
+  certificate: string;
+  //   amount: number;
+  //   status: "pending" | "processing" | "success" | "failed";
+  //   email: string;
+};
+
+export const AdminEventDataColumns: ColumnDef<AdminEvent>[] = [
+  // Row Selection
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+
+  //Data Accessors
+  {
+    accessorKey: "title",
+    //Sorting
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Title" />
+    ),
+  },
+  {
+    accessorKey: "courseLevel",
+    header: "Course Level",
+  },
+  {
+    accessorKey: "creditHour",
+    header: "Credit Hour",
+  },
+  {
+    accessorKey: "certificate",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Certificate
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+
+  // Data Action
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const adminEvent = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(adminEvent.id)}
+            >
+              Copy Admin Event ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
