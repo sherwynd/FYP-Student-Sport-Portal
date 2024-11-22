95ef01c816d73ad3fd7d1eb36742f898e37ef88d
Sherwynd Liew <sherwynd123@gmail.com>
2024-11-22 13:49:22 +0800

    Header optimised

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -4,33 +4,30 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>
-      <Flex
-        className="header-section"
-        color="black"
-        alignItems="center"
-        bg="blue.200"
-      >
-        <Image
-          src="/HeaderLogo.png"
-          width={80}
-          height={80}
-          alt=""
-          className="m-2"
-          priority={true}
-        />
-        <Spacer />
-        <Text fontSize="xl" display={{ base: "none", sm: "block" }}>
+    <header className="w-full bg-blue-300">
+      {/* Wrapper for 1440px constraint */}
+      <div className="mx-auto flex max-w-[1440px] items-center justify-between px-4 py-2">
+        <Flex alignItems="center">
+          <Image
+            src="/HeaderLogo.png"
+            width={80}
+            height={80}
+            alt="Logo"
+            className="m-2"
+            priority={true}
+          />
+          <Spacer />
+        </Flex>
+        <Text fontSize="xl" fontWeight="bold">
           Student Sport Portal
         </Text>
-
         <Spacer />
-        <Link href={`login`}>
+        <Link href="/login">
           <Button margin={{ base: "10px", sm: "20px", md: "30px" }}>
             Login
           </Button>
         </Link>
-      </Flex>
+      </div>
     </header>
   );
 }
