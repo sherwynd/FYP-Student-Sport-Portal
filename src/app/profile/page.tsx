28b78aa90524f934c6e78a8dc70aedc607459b56
Sherwynd Liew <sherwynd123@gmail.com>
2024-11-26 00:50:20 +0800

    profile added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,7 +4,6 @@ import { authOptions } from "@api/auth/[...nextauth]/options";
 
 export default async function Profile() {
   const session = await getServerSession(authOptions);
-  console.log("Session Data:", session);
 
   return (
     <>
@@ -15,15 +14,17 @@ export default async function Profile() {
         <div className="flex flex-col items-center justify-center bg-gray-100 py-10">
           <div className="w-96 rounded-lg bg-white p-6 text-center shadow-md">
             <div className="mb-4">
-              {session?.user?.image && (
-                <Image
-                  src={session.user?.image as string}
-                  alt="User Avatar"
-                  width={100}
-                  height={100}
-                  className="mx-auto rounded-full"
-                />
-              )}
+              <Image
+                src={
+                  session?.user?.image
+                    ? session.user.image
+                    : "/profile-template.png"
+                }
+                alt="User Avatar"
+                width={100}
+                height={100}
+                className="mx-auto rounded-full"
+              />
             </div>
             <h2 className="text-xl font-semibold text-gray-800">
               {session?.user?.name || "User Name"}
