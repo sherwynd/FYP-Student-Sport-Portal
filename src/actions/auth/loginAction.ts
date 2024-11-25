ebde4901897c54190841ed986fa78f13bdb429cd
Sherwynd Liew <sherwynd123@gmail.com>
2024-11-25 20:49:20 +0800

    changes in removing auth.ts and loginAction

--- src/actions/auth/loginAction.ts
+++ src/actions/auth/loginAction.ts
@@ -1,29 +1,22 @@
 "use server";
 import { redirect } from "next/navigation";
-import { signIn } from "@/auth";
-import { CredentialsSignin } from "next-auth";
+import { signIn } from "next-auth/react";
 
 export const login = async (formData: FormData) => {
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
 
   try {
-    if (email && password) {
-      redirect("/");
-    }
     await signIn("credentials", {
-      redirect: false,
-      callbackUrl: "/",
       email,
       password,
     });
+    redirect("/");
   } catch (error) {
-    const errorMessage = error as CredentialsSignin;
-    return errorMessage.cause;
+    throw new Error(error as string);
   }
-  redirect("/");
 };
 
 export const google = async () => {
-  await signIn("google");
+  // await signIn("google");
 };
