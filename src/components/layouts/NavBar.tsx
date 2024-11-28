52c71927a5aec06456c4f6f48a403a03c6385e38
Sherwynd Liew <sherwynd123@gmail.com>
2024-11-29 00:25:13 +0800

    added dynamic navbar witb role restriction

--- src/components/layouts/NavBar.tsx
+++ src/components/layouts/NavBar.tsx
@@ -1,27 +1,46 @@
 import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@api/auth/[...nextauth]/options";
+export default async function NavBar() {
+  const session = await getServerSession(authOptions);
 
-export default function NavBar() {
-  const links = [
+  const adminLinks = [
     { title: "Home", url: "/" },
     { title: "Event", url: "/event" },
     { title: "Profile", url: "/profile" },
     { title: "Admin", url: "/admin" },
-    { title: "Login", url: "/login" },
-    { title: "Register", url: "/register" },
+  ];
+
+  const links = [
+    { title: "Home", url: "/" },
+    { title: "Event", url: "/event" },
+    { title: "Profile", url: "/profile" },
   ];
 
   return (
     <nav className="flex min-h-[40px] items-center justify-center bg-black text-white">
       <div className="flex space-x-4">
-        {links.map(({ title, url }, index) => (
-          <Link
-            key={index}
-            href={url}
-            className="px-3 py-2 transition hover:text-gray-400"
-          >
-            {title}
-          </Link>
-        ))}
+        {session?.user?.role === "admin"
+          ? // If the user is logged in and is an admin
+            adminLinks.map(({ title, url }, index) => (
+              <Link
+                key={index}
+                href={url}
+                className="px-3 py-2 transition hover:text-gray-400"
+              >
+                {title}
+              </Link>
+            ))
+          : // If the user is logged in but not an admin
+            links.map(({ title, url }, index) => (
+              <Link
+                key={index}
+                href={url}
+                className="px-3 py-2 transition hover:text-gray-400"
+              >
+                {title}
+              </Link>
+            ))}
       </div>
     </nav>
   );
