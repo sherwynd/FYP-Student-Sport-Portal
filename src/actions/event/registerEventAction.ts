1091acd5aa1946e6b60da25064168bcae31a14ac
Sherwynd Liew <sherwynd123@gmail.com>
2024-12-07 23:02:11 +0800

    register Event will make active status

--- src/actions/event/registerEventAction.ts
+++ src/actions/event/registerEventAction.ts
@@ -26,6 +26,23 @@ export const registerEvent = async (eventId: string, userId: string) => {
     eventRegistrationId = data.id;
 
     createReportSubmission(eventId, userId, eventRegistrationId);
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: { isActive: true },
+    });
+
+    // Only update if not already active
+    if (!user?.isActive) {
+      await prisma.user.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          isActive: true,
+        },
+      });
+    }
   } catch (error) {
     console.error(error, "Error registering event");
   }
